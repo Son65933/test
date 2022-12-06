2c571a119c7cc521647078e0eedc0a95b2332b2f
Son65933 <119897183+Son65933@users.noreply.github.com>
2022-12-06 12:44:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import streamlist as st
 
-st.write('Hello World')
+#st.write('Hello World')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
